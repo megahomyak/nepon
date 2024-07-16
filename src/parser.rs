6e7afdd37b964caab611ec94daea194a68ea45ea
megahomyak <g.megahomyak@gmail.com>
2024-07-17 01:38:03 +0500

    Finished the parser

--- src/parser.rs
+++ src/parser.rs
@@ -19,20 +19,6 @@ pub enum Char {
     Regular(char),
 }
 
-pub fn escaped(s: S, is_bad: impl FnOnce(char) -> bool) -> Option<(Char, S)> {
-    match next(s) {
-        Some(('\\', s)) => next(s).map(|(c, s)| (Char::Escaped(c), s)),
-        Some((c, s)) => {
-            if is_bad(c) {
-                None
-            } else {
-                Some((Char::Regular(c), s))
-            }
-        }
-        None => None,
-    }
-}
-
 pub fn skip_whitespace(mut s: S) -> S {
     while let Some((c, new_s)) = next(s) {
         if !c.is_whitespace() {
@@ -46,17 +32,18 @@ pub fn skip_whitespace(mut s: S) -> S {
 /// [blah blah] (...)
 pub fn command_name(mut s: S) -> Option<(String, S)> {
     let mut result = String::new();
-    while let Some((c, new_s)) = escaped(s, |c| "()\n;".contains(c)) {
-        result.push(match c {
-            Char::Escaped(c) => c,
-            Char::Regular(c) => c,
-        });
+    while let Some((c, new_s)) = next(s) {
+        if "()\n;".contains(c) {
+            break;
+        }
+        result.push(c);
         s = new_s;
     }
     result = result.trim_end_matches(char::is_whitespace).to_owned();
     if result.is_empty() {
         None
     } else {
+        result.shrink_to_fit();
         Some((result, s))
     }
 }
@@ -65,43 +52,49 @@ pub fn command_name(mut s: S) -> Option<(String, S)> {
 pub fn input(mut s: S) -> Result<(String, S), Option<Error>> {
     let mut result = String::new();
     let mut paren_count = 1;
-    let str_before_next = s;
+    let str_at_beginning = s;
     match next(s) {
         Some(('(', new_s)) => s = new_s,
         _ => return Err(None),
     }
     loop {
-        match escaped(s, |c| "()".contains(c)) {
+        let str_at_current_char = s;
+        match next(s) {
             Some((c, new_s)) => {
-                match c {
-                    Char::Regular(c) => result.push(c),
-                    Char::Escaped(c) => {
-                        result.push('\\');
-                        result.push(c);
-                    }
-                }
                 s = new_s;
-            }
-            None => match next(s) {
-                Some((c, new_s)) => {
-                    if c == '(' {
+                match c {
+                    '(' => {
                         paren_count += 1;
                         result.push('(');
-                    } else {
+                    }
+                    ')' => {
                         paren_count -= 1;
                         if paren_count == 0 {
-                            return Ok((result, new_s));
+                            result.shrink_to_fit();
+                            return Ok((result, s));
                         }
                         result.push(')');
                     }
-                    s = new_s;
+                    '\\' => match next(s) {
+                        Some((c, new_s)) => {
+                            result.push('\\');
+                            result.push(c);
+                            s = new_s;
+                        }
+                        None => {
+                            return Err(Some(Error::NothingAfterEscapeCharacter {
+                                escape_character_index: str_at_current_char.index,
+                            }))
+                        }
+                    },
+                    _ => result.push(c),
                 }
-                None => {
-                    return Err(Some(Error::MissingClosingParen {
-                        index: str_before_next.index,
-                    }))
-                }
-            },
+            }
+            None => {
+                return Err(Some(Error::MissingClosingParen {
+                    index: str_at_beginning.index,
+                }))
+            }
         }
     }
 }
@@ -111,8 +104,7 @@ pub struct Expression {
     pub inputs: Vec<String>,
 }
 
-pub fn expression(s: S) -> Result<(Expression, S), Option<Error>> {
-    let mut s = skip_whitespace(s);
+pub fn expression(mut s: S) -> Result<(Expression, S), Option<Error>> {
     let str_before_command_name = s;
     let command_name = match command_name(s) {
         None => match next(s) {
@@ -133,7 +125,16 @@ pub fn expression(s: S) -> Result<(Expression, S), Option<Error>> {
         s = skip_whitespace(s);
         match input(s) {
             Err(Some(e)) => return Err(Some(e)),
-            Err(None) => return Ok((Expression { command_name, inputs }, s)),
+            Err(None) => {
+                inputs.shrink_to_fit();
+                return Ok((
+                    Expression {
+                        command_name,
+                        inputs,
+                    },
+                    s,
+                ))
+            }
             Ok((input, new_s)) => {
                 inputs.push(input);
                 s = new_s;
@@ -150,10 +151,28 @@ pub enum Error {
     MissingClosingParen { index: usize },
     InputWithoutCommandName { index: usize },
     UnexpectedClosingParen { index: usize },
+    NothingAfterEscapeCharacter { escape_character_index: usize },
 }
 
-pub fn program(s: S) -> Result<Program, Error> {
+pub fn program(mut s: S) -> Result<Program, Error> {
     let mut expressions = Vec::new();
-    // '\n' ';'
-    // check for something left behind. if any, that's UnexpectedClosingParen
+    loop {
+        s = skip_whitespace(s);
+        match expression(s) {
+            Err(Some(e)) => return Err(e),
+            Err(None) => match next(s) {
+                None => return Ok(Program { expressions }),
+                Some((c, new_s)) => {
+                    if !"\n;".contains(c) {
+                        return Err(Error::UnexpectedClosingParen { index: s.index });
+                    }
+                    s = new_s;
+                }
+            },
+            Ok((expression, new_s)) => {
+                expressions.push(expression);
+                s = new_s;
+            }
+        }
+    }
 }
